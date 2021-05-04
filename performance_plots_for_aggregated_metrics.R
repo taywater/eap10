8eb73197a25a5bf57243751960830298a3c4cafb
Nick <nmanna@akrf.com>
2021-05-04 11:02:53 -0400

    started this performance plot document

---
+++ performance_plots_for_aggregated_metrics.R
@@ -0,0 +1,7 @@
+#EAP10 
+#Performance plots for aggregated metrics
+#Looking for: aggregated and binned performance metrics; boxplots showing performance ranges for various storm size bins for select long-term SMPs, SMPs aggregated by functional type, SMPs aggregated by liner category; 
+#overtopping statsitcs presented as CDFs
+#porous pavement performance relative to rejuvenating maintenance events (do we already have this?)
+#other plots as time permits and insights present themselves
+
